c61ae3e3c3f7ecdc8afd0cdf02cfe2dd9e399791
OlegSuncrown <olegsuncrown@gmail.com>
2020-08-25 15:01:01 +0300

    Updates

---
+++ precache-manifest.4255b12ed2d5fb5ab86fa5165896b3ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b14947d1e96a22120cba8181c7a4d2",
+    "url": "/react-pagination/index.html"
+  },
+  {
+    "revision": "237affb25ce3f4dca631",
+    "url": "/react-pagination/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3abd1fb0e0d9ff04eda0",
+    "url": "/react-pagination/static/css/main.9e99c80c.chunk.css"
+  },
+  {
+    "revision": "237affb25ce3f4dca631",
+    "url": "/react-pagination/static/js/2.38d78695.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pagination/static/js/2.38d78695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3abd1fb0e0d9ff04eda0",
+    "url": "/react-pagination/static/js/main.04bb8152.chunk.js"
+  },
+  {
+    "revision": "43cfa14aaaaa9d70687b",
+    "url": "/react-pagination/static/js/runtime-main.ec41a5bf.js"
+  }
+]);
